ee8c10b7e648a00f18523fb0681772dc7cbc0643
Jeremy <jeremy.johns@yale.edu>
2026-02-06 10:00:17 -0700

    changes to dialect

--- dialect_profiles.js
+++ dialect_profiles.js
@@ -1,31 +1,69 @@
-// dialect_profiles.js
 const dialectProfiles = {
   akchin: {
-    name: "Ak-Chin",
-    features: [
-      "feature"
-    ],
-    phonemicInventoryCSV: "dialectprofiles/akchin/akchin_inventory.csv", // IPA chart CSV
-    grammaticalNotes: [
-      "feature"
-    ],
-    trajectoryMap: "",
-    tableCSV: ""  // optional word table
-  },
+    name: "Ak-Chin O'odham",
 
-  tohono: {
-    name: "Sells, Arizona",
-    features: [
-      "Dropping of some determiners",
-      "Unique vowel shifts"
-    ],
-    phonemicInventoryCSV: null,
-    grammaticalNotes: [
-      "Determiners often dropped sentence-initially",
-      "Reduplication signals aspect"
-    ],
-    trajectoryMap: null,
-    tableCSV: "tohono_dialect.csv"
+    metadata: {
+    },
+
+    phonology: {
+      phonemicInventoryCSV: "dialectprofiles/akchin/akchin_inventory.csv",
+      
+      notes: [
+        "information about dialect"// e.g. "Contrastive vowel length",
+        // e.g. "Restricted consonant clusters"
+      ],
+      allophony: [
+  {
+    label: "/v/ becomes [w] before the following sequences:",
+    items: [
+      "_ua: as in <b>wua</b> <em>(doing)</em>",
+      "_ui: as in <b>wui</b> <em>(to/toward)</em>",
+      "_uha: as in <b>wuha</b> <em>(woke up)</em>",
+      "_uhi: as in <b>wuhi</b> <em>(eye)</em>"
+    ]
+  },
+   {
+    label: "/h/ becomes breathy voice between two vowels, for example:",
+    items: [
+      "kahio sounds like <b>ka̤io</b> <em>(leg)</em>",
+      "daha sounds like <b>da̤a</b> <em>(sitting)</em>",
+      "wuhi sounds like <b>wṳi</b> <em>(eye)</em>"    
+    ]
+  },
+  {
+    label: "/ʔ/ (the glottal stop ') may become creaky voice between two vowels, for example:",
+    items: [
+      "do'ag sounds like <b>do̰ag</b> <em>(mountain)</em>",
+      "sa'i sounds like <b>sa̰i</b> <em>(bush)</em>",   
+    ]
+  },
+  {
+    label: "/ʔ/ (the glottal stop ') may also be deleted between two vowels, for example:",
+    items: [
+      "si'alim may sound like <b>sialim</b> <em>(morning)</em>",
+  
+    ]
+  } 
+ 
+]
+
+    },
+
+    grammar: {
+      morphology: [
+        // optional
+      ],
+      syntax: [
+        // optional
+      ]
+    },
+
+    lexicon: {
+      tableCSV: ""
+    },
+
+    maps: {
+      trajectory: ""
+    }
   }
 };
-
--- dialects.js
+++ dialects.js
@@ -7,10 +7,5 @@ const dialects = [
     lat: 33.0284,
     lng: -112.0876
   },
-  {
-    id: "tohono",
-    name: "Sells, Arizona",
-    lat: 31.9120,
-    lng: -111.8812
-  }
+
 ];
